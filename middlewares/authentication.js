a4bda4eff3a59b2a6abf63161a2b68420613453e
TheNolle <github@thenolle.com>
2023-04-28 10:26:36 +0200

    Login page and logic updated

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -3,12 +3,12 @@ import config from '../config.json' assert { type: 'json' }
 
 export function checkAuthentication(request, response, next) {
     const token = request.cookies.token
-    if (!token) return response.render('login', { errorCode: 2 })
+    if (!token) return response.redirect('/login')
     try {
         const decoded = jsonwebtoken.verify(token, config.app.secret)
         request.user = decoded
         next()
     } catch (err) {
-        response.render('login', { errorCode: 0 })
+        response.render('login', { errorCode: 2 })
     }
 }
